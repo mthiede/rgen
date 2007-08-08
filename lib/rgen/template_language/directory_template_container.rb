4d1322bf35dc076f5ca32f043903ae7b39707db7
thiedem <thiedem@9d6b4941-6e19-0410-8ae8-cc58abb52b0c>
2007-08-08 20:00:45 +0000

    better error messages from templates, separator feature, improved whitespace handling

--- lib/rgen/template_language/directory_template_container.rb
+++ lib/rgen/template_language/directory_template_container.rb
@@ -5,47 +5,65 @@ require 'rgen/template_language/template_container'
 require 'rgen/template_language/template_helper'
 
 module RGen
-
+  
 module TemplateLanguage
-
+    
 class DirectoryTemplateContainer
-	include TemplateHelper
-	
-	def initialize(metamodel=nil, output_path=nil, parent=nil)
-		@containers = {}
-		@parent = parent
-		@metamodel = metamodel
-		@output_path = output_path
-	end
-	
-	def load(dir)
-		#print "Loading templates in #{dir} ...\n"
-		Dir.foreach(dir) { |f|
-			qf = dir+"/"+f
-			if !File.directory?(qf) && f =~ /^(.*)\.tpl$/
-				(@containers[$1] = TemplateContainer.dup.new(@metamodel, @output_path, self)).load(qf)
-			elsif File.directory?(qf) && f != "." && f != ".."
-				(@containers[f] = DirectoryTemplateContainer.new(@metamodel, @output_path, self)).load(qf)
-			end
-		}
-	end
-	
-	def expand(template, *all_args)
-		args, params = _splitArgsAndOptions(all_args)
-		element = params[:for]
-		if template =~ /^\// && @parent
-			@parent.expand(template, *all_args)
-		elsif template =~ /^[\/]*(\w+)[:\/]+(.*)/ 
-			throw "Template not found: #{$1}" unless @containers[$1]
-			@containers[$1].expand($2, *all_args)
-		elsif @parent
-			@parent.expand(template, *all_args)
-		else
-			throw "Template not found: #{template}"
-		end
-	end
+  include TemplateHelper
+  
+  def initialize(metamodel=nil, output_path=nil, parent=nil)
+    @containers = {}
+    @parent = parent
+    @metamodel = metamodel
+    @output_path = output_path
+  end
+  
+  def load(dir)
+    #print "Loading templates in #{dir} ...\n"
+    Dir.foreach(dir) { |f|
+      qf = dir+"/"+f
+      if !File.directory?(qf) && f =~ /^(.*)\.tpl$/
+       (@containers[$1] = TemplateContainer.dup.new(@metamodel, @output_path, self,qf)).load
+      elsif File.directory?(qf) && f != "." && f != ".."
+       (@containers[f] = DirectoryTemplateContainer.new(@metamodel, @output_path, self)).load(qf)
+      end
+    }
+  end
+  
+  def expand(template, *all_args)
+    if template =~ /^\//
+      if @parent
+        # pass to parent
+        @parent.expand(template, *all_args)
+      else
+        # this is root
+        _expand(template, *all_args)
+      end
+    elsif template =~ /^\.\.\/(.*)/
+      if @parent
+        # pass to parent
+        @parent.expand($1, *all_args)
+      else
+        raise "No parent directory for root"
+      end
+    else
+      _expand(template, *all_args)
+    end
+  end
+  
+  private
+  
+  def _expand(template, *all_args)
+    if template =~ /^\/?([^:\/]+)(?:::|\/)([^:\/].*)/ 
+      raise "Template not found: #{$1}" unless @containers[$1]
+      @containers[$1].expand($2, *all_args)
+    else
+      raise "Invalid template name: #{template}"
+    end
+  end
+  
 end
 
 end
-
+    
 end
--- lib/rgen/template_language/output_handler.rb
+++ lib/rgen/template_language/output_handler.rb
@@ -2,83 +2,99 @@
 # (c) Martin Thiede, 2006
 
 module RGen
-
+  
 module TemplateLanguage
-
-class OutputHandler
-	attr_writer :indent
-	
-	def initialize(indent=0, mode=:explicit)
-		self.mode = mode
-		@indent = indent
-		@state = :wait_for_nonws
-		@output = ""
-	end
-		
-	def concat(s)
-		return @output.concat(s) if s.is_a? OutputHandler
-		s = s.to_str.gsub(/^[\t ]*\r?\n/,'') if @ignoreNextNL
-		s = s.to_str.gsub(/^\s+/,'') if @ignoreNextWS
-		@ignoreNextNL = @ignoreNextWS = false if s =~ /\S/
-		if @mode == :direct
-			@output.concat(s)
-		elsif @mode == :explicit
-			while s.size > 0
-				#puts "DEGUB: #{@state} #{s.dump}"
-				# s starts with whitespace
-				if s =~ /\A(\s+)(.*)/m	
-					ws = $1; rest = $2
-					#puts "DEGUB: ws #{ws.dump} rest #{rest.dump}"
-					if @state == :wait_for_nl
-						# ws contains a newline
-						if ws =~ /\A[\t ]*(\r?\n)(\s*)/m
-							@output.concat($1)
-							@state = :wait_for_nonws
-							s = $2 + rest
-						else
-							@output.concat(ws)
-							s = rest
-						end
-					else
-						s = rest
-					end
-				# s starts with non-whitespace
-				elsif s =~ /\A(\S+)(.*)/m
-					nonws = $1; rest = $2
-					#puts "DEGUB: nonws #{nonws.dump} rest #{rest.dump}"
-					@output.concat("   "*@indent) if @state == :wait_for_nonws
-					@output.concat(nonws)
-					@state = :wait_for_nl
-					s = rest
-				end
-			end
-		end
-	end
-	alias << concat
-	
-	def to_str
-		@output
-	end
-	alias to_s to_str
-
-	def direct_concat(s)
-		@output.concat(s)
-	end
-			
-	def ignoreNextNL
-		@ignoreNextNL = true
-	end
-	
-	def ignoreNextWS
-		@ignoreNextWS = true
-	end
-	
-	def mode=(m)
-		raise StandardError.new("Unknown mode: #{m}") unless [:direct, :explicit].include?(m)
-		@mode = m
-	end
+  
+  class OutputHandler
+    attr_writer :indent
+    
+    def initialize(indent=0, mode=:explicit)
+      self.mode = mode
+      @indent = indent
+      @state = :wait_for_nonws
+      @output = ""
+    end
+    
+    # ERB will call this method for every string s which is part of the
+    # template file in between %> and <%. If s contains a newline, it will
+    # call this method for every part of s which is terminated by a \n
+    # 
+    def concat(s)
+      return @output.concat(s) if s.is_a? OutputHandler
+      s = s.to_str.gsub(/^[\t ]*\r?\n/,'') if @ignoreNextNL
+      s = s.to_str.gsub(/^\s+/,'') if @ignoreNextWS
+      @ignoreNextNL = @ignoreNextWS = false if s =~ /\S/
+      if @mode == :direct
+        @output.concat(s)
+      elsif @mode == :explicit
+        while s.size > 0
+          #puts "DEGUB: #{@state} #{s.dump}"
+          # s starts with whitespace
+          if s =~ /\A(\s+)(.*)/m	
+            ws = $1; rest = $2
+            #puts "DEGUB: ws #{ws.dump} rest #{rest.dump}"
+            if @state == :wait_for_nl
+              # ws contains a newline
+              if ws =~ /\A[\t ]*(\r?\n)(\s*)/m
+                @output.concat($1)
+                @state = :wait_for_nonws
+                s = $2 + rest
+              else
+                @output.concat(ws)
+                s = rest
+              end
+            else
+              s = rest
+            end
+            # s starts with non-whitespace
+          elsif s =~ /\A(\S+)(.*)/m
+            nonws = $1; rest = $2
+            #puts "DEGUB: nonws #{nonws.dump} rest #{rest.dump}"
+            if @state == :wait_for_nonws
+              # within the same output handle we can recognize a newline by ourselves
+              # but if the output handler is changed, someone has to tell us
+              if !@noIndentNextLine && !(@output =~ /[^\n]\z/)
+                @output.concat("   "*@indent)
+              else
+                @noIndentNextLine = false
+              end
+            end
+            @output.concat(nonws)
+            @state = :wait_for_nl
+            s = rest
+          end
+        end
+      end
+    end
+    alias << concat
+    
+    def to_str
+      @output
+    end
+    alias to_s to_str
+    
+    def direct_concat(s)
+      @output.concat(s)
+    end
+    
+    def ignoreNextNL
+      @ignoreNextNL = true
+    end
+    
+    def ignoreNextWS
+      @ignoreNextWS = true
+    end
+    
+    def noIndentNextLine
+      @noIndentNextLine = true
+    end  
+      
+    def mode=(m)
+      raise StandardError.new("Unknown mode: #{m}") unless [:direct, :explicit].include?(m)
+      @mode = m
+    end
+  end
+  
 end
-
-end
-
+  
 end
